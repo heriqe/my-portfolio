8ede9d9724b8d57d198c032b3850ff40e8b55ae8
Henrique Ferreira da Silva <00001213449856sp@al.educacao.sp.gov.br>
2025-06-09 23:08:34 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Henrique</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,8 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        <h1>Bem-vindo ao meu portfólio!</h1>
-      <p>Este é um exemplo de conteúdo estático no index.html.</p>
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
